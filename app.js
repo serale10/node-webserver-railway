85d5c2e379cc65d8741dd6d0c78fccc81d9eec63
serale10 <103468809+serale10@users.noreply.github.com>
2022-12-14 10:26:50 -0600

    webserver listo

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT
+
+
+// Handlebars
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+// servir contenido estático
+
+app.use( express.static('public') );
+
+app.get('/', function (req, res) {
+    res.render('home', {
+        nombre: 'Fernando Herrera',
+        titulo: 'Curso Node'
+    });
+  });
+
+app.get('/generic', function (req, res) {
+    res.render('generic')
+});
+
+app.get('/elements', function (req, res) {
+    res.render('elements')
+});
+
+app.get('/generic', function (req, res) {
+    res.sendfile(__dirname + '/public/generic.html')
+  });
+
+app.get('/elements', function (req, res) {
+res.sendfile(__dirname + '/public/elements.html')
+});
+
+app.get('*', function (req, res) {
+    res.sendfile(__dirname + '/public/404.html')
+});
+
+
+app.listen( port, () =>{
+    console.log(`Example app listening at http://localhostL:${port}`);
+});
